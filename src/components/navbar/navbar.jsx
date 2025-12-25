f12d2f49ec092367099d2b2cc0314ccccc7616dc
Asim Sk <blackhatcode786@gmail.com>
2025-12-25 14:22:11 +0530

    Remove unused Sheet component

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <Logo />
 
         {/* Desktop Menu */}
-        <NavMenu className="hidden md:block" />
+        <NavMenu />
 
         <div className="flex items-center gap-3">
           <ThemeToggle />
